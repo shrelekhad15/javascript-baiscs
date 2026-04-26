24d1e692f334dedf58b2e3fa63ace161a1260e34
shrelekhad15 <shrelekhad@gmail.com>
2026-04-26 11:16:45 +0000

    done

---
+++ 01_basics/datatypes2.js
@@ -0,0 +1,22 @@
+let score = 12
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+/* "12"=12
+"12abc"=NaN
+true= 1; false=0;
+*/
+
+let isLoggedIn = "shree"
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanisLoggedIn);
+
+/*
+""=false
+"shree"=true
+1= true 0=false
+*/ 
