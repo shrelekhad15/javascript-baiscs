12834ce4df7b14651718a82a84531ee1a8b46558
shrelekhad15 <shrelekhad@gmail.com>
2026-04-26 19:51:10 +0000

    done

---
+++ 01_basics/memory.js
@@ -0,0 +1,20 @@
+
+
+let ename="shree"
+let anotherName=ename
+anotherName="jss"
+
+console.log(ename);
+console.log(anotherName);
+
+let userOne={
+    email:"shree@gmail.com"
+
+}
+let userTwo=userOne
+userTwo.email="shreee@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
---
+++ 01_basics/string.js
@@ -0,0 +1,36 @@
+const name="shree"
+const repoCount=50
+
+//console.log(name + repoCount + "value")
+
+console.log('hello my name is ${name} and my repo count is ${repoCount}');
+
+
+const gameNmae= new string ('hitesh-hc-com')
+
+//console.log(gameName[0])
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length)
+//console.log(gameName.toUppercase())
+
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('t'))
+
+const newString=  gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
